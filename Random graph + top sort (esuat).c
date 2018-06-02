7d1019f97215fb81905428a855c8d4df1580bd3e
Chandler1215 <39851804+Chandler1215@users.noreply.github.com>
2018-06-02 13:10:36 +0300

    Update Random graph + top sort (esuat).c

--- Random graph + top sort (esuat).c
+++ Random graph + top sort (esuat).c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MIN_PER_RANK 1 /* Nodes/Rank: How 'fat' the DAG should be.  */
+#define MIN_PER_RANK 1
 #define MAX_PER_RANK 5
-#define MIN_RANKS 3    /* Ranks: How 'tall' the DAG should be.  */
+#define MIN_RANKS 3    
 #define MAX_RANKS 5
-#define PERCENT 30     /* Chance of having an Edge.  */
+#define PERCENT 30    
 
 struct graph{
     int no_elements;
@@ -61,17 +61,17 @@ void random_graph( struct graph *graph ){
   printf ("digraph ");
   for (i = 0; i < ranks; i++)
     {
-      /* New nodes of 'higher' rank than all nodes generated till now.  */
+     
       int new_nodes = MIN_PER_RANK + (rand () % (MAX_PER_RANK - MIN_PER_RANK + 1));
 
-      /* Edges from old nodes ('nodes') to new ones ('new_nodes').  */
+   
       for (j = 0; j < nodes; j++)
         for (k = 0; k < new_nodes; k++)
           if ( (rand () % 100) < PERCENT){
             graph -> ad_matrix[j][k + nodes] = 1;
           }
 
-      nodes += new_nodes; /* Accumulate into old node set.  */
+      nodes += new_nodes; 
     }
 }
 
